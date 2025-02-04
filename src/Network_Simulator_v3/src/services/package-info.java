7f759471c5538332b1741bef730548fb5a8534a8
Pantelis Rodis <prodis@admin.grnet.gr>
2025-02-04 10:29:05 +0200

    04/02/2025 update

--- src/Network_Simulator_v3/src/services/package-info.java
+++ src/Network_Simulator_v3/src/services/package-info.java
@@ -1,9 +1,9 @@
 /** Implementations of network services.
+ * The "Broker" class implements a broker that partitions HDA-graphs.
  * The "HDA" Hyper-Distributed Application class.
  * The "HDAgraph" class implements a Hyper-Distributed Application graph.
+ * The "HDAconfiguration" class simulates a HDA configuration.
  * The "SFC" Service Function Chain class.
  * The "VNFgraph" class implements a Virtual Network Function graph.
- * The "HDAconfiguration" class simulates a HDA configuration.
- * The "Broker" class implements a broker that partitions VNF-graphs.
  */
 package services;
